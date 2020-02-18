0eae9fc323b360228d0d5076bac244f8f9611a40
Amit Galitzky <57645112+amitGenemod@users.noreply.github.com>
2020-02-17 20:32:50 -0800

    leaflet works and you get bounds from it

--- js/script.js
+++ js/script.js
@@ -3,14 +3,31 @@ let state = {
     chosenInfoItem: {},
     inputtedText: ''
 }
+let myMap = L.map('leaflet-map').setView([47.606209, -122.332069], 10);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW1pdDE3IiwiYSI6ImNrNnJibmF2bzA0ZXgzbG11dzNkcmh5YWsifQ.tfSRkB3YoUJPPIlc0UxuZQ', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiYW1pdDE3IiwiYSI6ImNrNnJibmF2bzA0ZXgzbG11dzNkcmh5YWsifQ.tfSRkB3YoUJPPIlc0UxuZQ'
+}).addTo(myMap);
 
 
 
-function callDataByAmenityOverPass() {
-    
-    amenity = document.getElementById("search").value;
+function callDataByAmenityOverPass(amenityFromClient) {
+    let strLatLong = mapBoundOverpass();
 
-    fetch('https://www.overpass-api.de/api/interpreter?data=[out:json];node[amenity=' + amenity + '](47.481002,-122.459696,47.734136,-122.224433);out%20meta;')
+    let amenity = amenityFromClient;
+    let listDiv = document.getElementById("left-view-list");
+    listDiv.style.display = "block";
+
+    let homeDiv = document.getElementById("left-view-home");
+    homeDiv.style.display = "none";
+
+    // amenity = document.getElementById("search").value;
+
+    fetch('https://www.overpass-api.de/api/interpreter?data=[out:json];node[amenity=' + amenity + '](' + strLatLong + ');out%20meta;')
         .then((response) => {
             return response.json();
         })
@@ -19,36 +36,51 @@ function callDataByAmenityOverPass() {
         });
 }
 
-// function callDataByName() {
-    
-//     name = document.getElementById("search").value;
+function mapBoundNomi() {
+    let latlong = [];
+    latlong.push(myMap.getBounds()._southWest);
+    latlong.push(myMap.getBounds()._northEast);
 
-//     fetch('https://www.overpass-api.de/api/interpreter?data=[out:json];node[name="' + name + '"](47.481002,-122.459696,47.734136,-122.224433);out%20meta;')
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .then((myJson) => {
-//             console.log(myJson.elements);
-//         });
-// }
+    let strLatLong = "" + latlong[0].lng + "," + latlong[0].lat + "," + latlong[1].lng + "," + latlong[1].lat;
+    return strLatLong;
+
+
+}
+
+function mapBoundOverpass() {
+    let latlong = [];
+    latlong.push(myMap.getBounds()._southWest);
+    latlong.push(myMap.getBounds()._northEast);
+    console.log(latlong);
+
+    let strLatLong = "" + latlong[0].lat + "," + latlong[0].lng + "," + latlong[1].lat + "," + latlong[1].lng;
+    return strLatLong;
+
+}
 
 function callDataByName() {
 
+    let strLatLong = mapBoundNomi();
+ 
     let listDiv = document.getElementById("left-view-list");
     listDiv.style.display = "block";
 
     let homeDiv = document.getElementById("left-view-home");
     homeDiv.style.display = "none";
-    
+    let data = {};
+
     name = document.getElementById("search").value;
     //https://nominatim.openstreetmap.org/search?q=target&format=json&viewbox=-122.459696,47.481002,-122.224433,47.734136&bounded=1
-    fetch('https://nominatim.openstreetmap.org/search?q=' + name +'&format=json&viewbox=-122.459696,47.481002,-122.224433,47.734136&bounded=1&extratags=1')
+    fetch('https://nominatim.openstreetmap.org/search?q=' + name + '&format=json&viewbox=' + strLatLong + '&bounded=1&extratags=1')
         .then((response) => {
             return response.json();
         })
         .then((myJson) => {
             console.log(myJson);
+            data = myJson;
         });
+
+    console.log(data);
 }
 
 function toggleDisplayInfo() {
@@ -62,7 +94,7 @@ function toggleDisplayInfo() {
 
 
 // function callDataByName() {
-    
+
 //     name = document.getElementById("search").value;
 //     https://nominatim.openstreetmap.org/search?q=target&format=json&viewbox=-122.459696,47.481002,-122.224433,47.734136&bounded=1
 //     fetch('https://nominatim.openstreetmap.org/search?q=[' + name +']&format=json&viewbox=-122.459696,47.481002,-122.224433,47.734136&bounded=1&extratags=1')
@@ -74,15 +106,27 @@ function toggleDisplayInfo() {
 //         });
 // }
 
-// var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: 'your.mapbox.access.token'
-// }).addTo(mymap);
+
+// let myMap = () => L.map('leaflet-map').setView([47.606209, -122.332069], 10).then(myMap => {
+//     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//         maxZoom: 18,
+//         id: 'mapbox/streets-v11',
+//         tileSize: 512,
+//         zoomOffset: -1,
+//         accessToken: 'your.mapbox.access.token'
+//     }).addTo(mymap);
+// })
+
+// let map = document.getElementById("map-outer");
+// let leaflet = document.createElement('div');
+// leaflet.classList.add('leaflet-map');
+// map.appendChild(leaflet).
+
+
+
+
+
 
 
 // function displayDivs {
