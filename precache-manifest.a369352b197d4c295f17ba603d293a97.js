66df9ebf83b35960ae75c7857028793a47509958
Amit Galitzky <57645112+amitGenemod@users.noreply.github.com>
2020-03-04 18:44:38 -0800

    Updates

---
+++ precache-manifest.a369352b197d4c295f17ba603d293a97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f4ddb98545349449bf40919d7d5a01",
+    "url": "/project-accessibilityfinder/index.html"
+  },
+  {
+    "revision": "d3b3f17905e98100a639",
+    "url": "/project-accessibilityfinder/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7bac69f86cba051b39bb",
+    "url": "/project-accessibilityfinder/static/css/main.672d111b.chunk.css"
+  },
+  {
+    "revision": "d3b3f17905e98100a639",
+    "url": "/project-accessibilityfinder/static/js/2.f1d29d38.chunk.js"
+  },
+  {
+    "revision": "4c8846796f6101f120aa42a5841fba68",
+    "url": "/project-accessibilityfinder/static/js/2.f1d29d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bac69f86cba051b39bb",
+    "url": "/project-accessibilityfinder/static/js/main.952e6d7b.chunk.js"
+  },
+  {
+    "revision": "dafc57f24d22d048b895",
+    "url": "/project-accessibilityfinder/static/js/runtime-main.b72bf324.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/project-accessibilityfinder/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.63a48e0c61e6daea92a73f21da0b1899.js"
+  "/project-accessibilityfinder/precache-manifest.a369352b197d4c295f17ba603d293a97.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-accessibilityfinder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
