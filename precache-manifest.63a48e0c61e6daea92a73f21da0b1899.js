b0171a051ea0c9f14beab3ea023132e45465446e
Amit Galitzky <57645112+amitGenemod@users.noreply.github.com>
2020-03-04 18:41:37 -0800

    Updates

---
+++ precache-manifest.63a48e0c61e6daea92a73f21da0b1899.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff63292ba201f03ab3dec15c620358b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3b3f17905e98100a639",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a3df4a19ca41596eb7b1",
+    "url": "/static/css/main.672d111b.chunk.css"
+  },
+  {
+    "revision": "d3b3f17905e98100a639",
+    "url": "/static/js/2.f1d29d38.chunk.js"
+  },
+  {
+    "revision": "4c8846796f6101f120aa42a5841fba68",
+    "url": "/static/js/2.f1d29d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3df4a19ca41596eb7b1",
+    "url": "/static/js/main.1b62f31a.chunk.js"
+  },
+  {
+    "revision": "8095970f76fad6608c7c",
+    "url": "/static/js/runtime-main.fe112b11.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
